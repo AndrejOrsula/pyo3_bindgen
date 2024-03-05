b3e305d344fb75e94c84a549d835fa5fe7f560cb
Andrej Orsula <orsula.andrej@gmail.com>
2024-03-05 23:30:37 +0100

    Make Config fields public
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- pyo3_bindgen_engine/src/config.rs
+++ pyo3_bindgen_engine/src/config.rs
@@ -20,51 +20,51 @@ const DEFAULT_BLOCKLIST_ATTRIBUTE_NAMES: [&str; 4] = ["builtins", "testing", "te
 pub struct Config {
     /// Flag that determines whether to recursively generate code for all submodules of the target modules.
     #[builder(default = true)]
-    pub(crate) traverse_submodules: bool,
+    pub traverse_submodules: bool,
 
     /// Flag that determines whether to generate code for prelude modules (Python `__all__` attribute).
     #[builder(default = true)]
-    pub(crate) generate_preludes: bool,
+    pub generate_preludes: bool,
     /// Flag that determines whether to generate code for imports.
     #[builder(default = true)]
-    pub(crate) generate_imports: bool,
+    pub generate_imports: bool,
     /// Flag that determines whether to generate code for classes.
     #[builder(default = true)]
-    pub(crate) generate_classes: bool,
+    pub generate_classes: bool,
     /// Flag that determines whether to generate code for type variables.
     #[builder(default = true)]
-    pub(crate) generate_type_vars: bool,
+    pub generate_type_vars: bool,
     /// Flag that determines whether to generate code for functions.
     #[builder(default = true)]
-    pub(crate) generate_functions: bool,
+    pub generate_functions: bool,
     /// Flag that determines whether to generate code for properties.
     #[builder(default = true)]
-    pub(crate) generate_properties: bool,
+    pub generate_properties: bool,
     /// Flag that determines whether to documentation for the generate code.
     /// The documentation is based on Python docstrings.
     #[builder(default = true)]
-    pub(crate) generate_docs: bool,
+    pub generate_docs: bool,
 
     /// List of blocklisted attribute names that are skipped during the code generation.
     #[builder(default = DEFAULT_BLOCKLIST_ATTRIBUTE_NAMES.iter().map(|&s| s.to_string()).collect())]
-    pub(crate) blocklist_names: Vec<String>,
+    pub blocklist_names: Vec<String>,
     /// Flag that determines whether private attributes are considered while parsing the Python code.
     #[builder(default = false)]
-    pub(crate) include_private: bool,
+    pub include_private: bool,
 
     /// Flag that determines whether to generate code for all dependencies of the target modules.
     /// The list of dependent modules is derived from the imports of the target modules.
     ///
     /// Warning: This feature is not fully supported yet.
     #[builder(default = false)]
-    pub(crate) generate_dependencies: bool,
+    pub generate_dependencies: bool,
 
     /// Flag that suppresses the generation of Python STDOUT while parsing the Python code.
     #[builder(default = true)]
-    pub(crate) suppress_python_stdout: bool,
+    pub suppress_python_stdout: bool,
     /// Flag that suppresses the generation of Python STDERR while parsing the Python code.
     #[builder(default = true)]
-    pub(crate) suppress_python_stderr: bool,
+    pub suppress_python_stderr: bool,
 }
 
 impl Default for Config {
